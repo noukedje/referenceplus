f00fb83231ac50da4226d2df587e1b8d204bd690
noukedje <110611859+noukedje@users.noreply.github.com>
2022-08-06 10:46:19 +0100

    Delete Repository directory

--- Repository/PanierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestion.commerce.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gestion.commerce.Model.Panier;
-
-public interface PanierRepository extends JpaRepository<Panier,Long>{
-   //public Panier findBySessionToken(String sessionToken);
-}
